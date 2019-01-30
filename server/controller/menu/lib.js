35d55b5b0ab2dd42f47add652a72caf6119574da
Gwenn LINSKI <gwenn.linski@gmail.com>
2019-01-30 14:59:53 +0100

    structuration du controller menu pour l'adapter au controllerLoader et au router

---
+++ server/controller/menu/lib.js
@@ -0,0 +1,20 @@
+const Menu = require('../../models/menu')
+
+function addMenu(req, res) {
+    var menu = new Menu({
+        name: req.body.name,
+        entree: req.body.entree,
+        plat: req.body.plat,
+        dessert: req.body.dessert,
+        day: req.body.day,
+        price: req.body.price,
+    });
+
+    menu.save().then(doc => {
+        res.status(200).send(doc);
+    }).catch(err => {
+        res.status(400).send(err);
+    })
+}
+
+exports.addMenu = addMenu;
--- server/controller/menuController.js
+++ server/controller/menuController.js
@@ -1,19 +1,6 @@
-const { Menu } = require('./models/menu');
+const menus = require('./menu/lib')
 
-// POST /menus
-app.post('/menus', (req, res) => {
-    var menu = new Menu({
-        name: req.body.name,
-        entree: req.body.entree,
-        plat: req.body.plat,
-        dessert: req.body.dessert,
-        day: req.body.day,
-        price: req.body.price,
-    });
-
-    menu.save().then(doc => {
-        res.status(200).send(doc);
-    }).catch(err => {
-        res.status(400).send(err);
-    })
-})
+module.exports = function (app) {
+    // POST /menus
+    app.post('/menus', menus.addMenu)
+}
