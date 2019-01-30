8b8304002c3ea0733a4456c4aed8efcf23cdee37
Gwenn LINSKI <gwenn.linski@gmail.com>
2019-01-30 15:45:07 +0100

    fin du controller menu

--- server/controllers/menu/lib.js
+++ server/controllers/menu/lib.js
@@ -1,5 +1,7 @@
-const Menu = require('../../models/menu')
+const Menu = require('../../models/menu');
+const {ObjectID} = require('mongodb');
 
+// CREATE
 function addMenu(req, res) {
     var menu = new Menu({
         name: req.body.name,
@@ -16,9 +18,71 @@ function addMenu(req, res) {
         res.status(400).send(err);
     })
 }
-function getMenu(req, res) {
-    res.status(200).send({text: "getMenu"})
+
+// READ
+function getMenus(req, res) {
+    Menu.find().then(menus => {
+        res.send({menus})
+    }).catch(err => {
+        res.status(400).send(err)
+    }) 
+}
+
+function getOneMenu(req, res) {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+    Menu.findById(id).then(menu => {
+        if (!menu) {
+            return res.status(404).send()
+        }
+        res.status(200).send({todo})
+    })
+    .catch( err => {
+        res.status(400).send(err)
+    })
+}
+
+// UPDATE
+function editMenu(req, res) {  
+    var id = req.params.id;
+    var body = _.pick(req.body, ['name', 'entree', 'plat', 'dessert', 'day', 'price'])
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Menu.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then(menu => {
+            if(!menu) {
+                return res.status(404).send()
+            }
+            res.status(200).send({menu});
+        }).catch(err => res.status(400).send())
+}
+
+// DELETE
+function deleteMenu(req, res) {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+    Menu.findByIdAndDelete(id).then(menu => {
+        //findByIdAndDelete renvoie l'objet supprimé
+        if(!menu) {
+            return res.status(404).send();
+        }
+        res.status(200).send({menu});
+    }).catch( err => {
+        res.status(400).send()
+    })
     
 }
 
+
 exports.addMenu = addMenu;
+exports.getMenus = getMenus;
+exports.getOneMenu = getOneMenu;
+exports.editMenu = editMenu;
+exports.deleteMenu = deleteMenu;
--- server/controllers/routes/menuController.js
+++ server/controllers/routes/menuController.js
@@ -3,4 +3,16 @@ const menus = require('../menu/lib')
 module.exports = function (app) {
     // POST /menus
     app.post('/menus', menus.addMenu)
+
+    //GET /menus
+    app.get('/menus', menus.getMenus)
+
+    // GET /menus/id
+    app.get('/menus/:id', menus.getOneMenu)
+
+    // PATCH /menus/id
+    app.patch('/menus/:id', menus.editMenu)
+
+    // DELETE /menus/id
+    app.delete('/menus/:id', menus.deleteMenu)
 }
