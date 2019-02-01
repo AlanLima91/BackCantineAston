748a1fecccb7fabbdcd97100f6d6a618921f6169
Alan <m.limaalan@hotmail.fr>
2019-02-01 14:44:12 +0100

    Token add to attribute

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var app = express();
 // middleware décodant le json inclu dans le body des  requêtes
 app.use(bodyParser.json());
 
+// middleware pour les CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
